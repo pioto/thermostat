428dbafc25478b3efe8ecff6827386fe36795cde
Mike Kelly <pioto@pioto.org>
2015-08-04 00:50:04 -0400

    Factor out a single RestTemplate instance

--- src/main/java/org/pioto/thermostat/services/ThermostatServiceImpl.java
+++ src/main/java/org/pioto/thermostat/services/ThermostatServiceImpl.java
@@ -10,19 +10,17 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class ThermostatServiceImpl implements ThermostatService {
 
+	private final RestTemplate restTemplate = new RestTemplate();
+
 	@Cacheable("tstat")
 	@Override
 	public Tstat getTstat(ThermostatDevice device) {
-		RestTemplate restTemplate = new RestTemplate();
-
 		return restTemplate.getForObject(device.getBaseUrl() + "/tstat",
 				Tstat.class);
 	}
 
 	@Override
 	public PostResult postTstat(ThermostatDevice device, Tstat tstat) {
-		RestTemplate restTemplate = new RestTemplate();
-
 		return restTemplate.postForObject(device.getBaseUrl() + "/tstat",
 				tstat, PostResult.class);
 	}
